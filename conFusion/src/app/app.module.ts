cb673b425a2dd89ff23446fe60a7a5a9ff8ce4bd
Kakoty, S <s.kakoty@accenture.com>
2018-11-04 20:10:26 +0530

    Template Forms Part 1

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -29,6 +29,12 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
 import { LoginComponent } from './login/login.component';
 import { MatDialogModule } from '@angular/material/dialog';
 
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FormsModule } from '@angular/forms'; 
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -51,7 +57,11 @@ import { MatDialogModule } from '@angular/material/dialog';
     MatGridListModule,
     MatCardModule,
     MatButtonModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatFormFieldModule, 
+    MatInputModule,
+    MatCheckboxModule,
+    FormsModule 
   ],
   providers: [DishService, PromotionService, LeaderService],
   bootstrap: [AppComponent],
