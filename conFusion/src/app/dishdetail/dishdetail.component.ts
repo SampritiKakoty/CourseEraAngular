b63b4b3e87825f2ab897ed0b6ef977e3e108f746
Kakoty, S <s.kakoty@accenture.com>
2018-11-19 20:35:02 +0530

    Saving Changes to Server

--- conFusion/src/app/dishdetail/dishdetail.component.ts
+++ conFusion/src/app/dishdetail/dishdetail.component.ts
@@ -25,6 +25,7 @@ export class DishdetailComponent implements OnInit {
   prev: string;
   next: string;
   errMess: string;
+  dishcopy: Dish;
  
 
   commentForm: FormGroup;
@@ -61,7 +62,7 @@ export class DishdetailComponent implements OnInit {
 
    this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
    this.route.params.pipe(switchMap((params: Params) => this.dishservice.getDish(params['id'])))
-   .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); },
+   .subscribe(dish => { this.dish = dish; this.dishcopy = dish; this.setPrevNext(dish.id); },
    errmess => this.errMess = <any>errmess);
 
    this.commentForm = this.fb.group({
@@ -113,7 +114,13 @@ export class DishdetailComponent implements OnInit {
     let date: Date = new Date();
     this.comment = this.commentForm.value;
     this.comment.date = date.toISOString();
-    this.dish.comments.push(this.comment);
+    this.dishcopy.comments.push(this.comment);
+    this.dishservice.putDish(this.dishcopy)
+    .subscribe(dish => {
+      this.dish = dish; this.dishcopy = dish;
+    },
+    errmess => { this.dish = null; this.dishcopy = null; this.errMess = <any>errmess; });
+    //this.dish.comments.push(this.comment);
     this.commentForm.reset({
       firstname: '',
       lastname: '',
